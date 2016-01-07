79227ed57445b4b3b6b6a5bb32085478d77a8528
Amit Upadhyay <comments@amitu.com>
2016-01-07 18:08:04 +0530

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="ipython-autotime",
+    description="Time everything in IPython",
+
+    version="0.1",
+
+    author="Phillip Cloud,
+    author_email="cpcloud@gmail.com",
+
+    url="https://github.com/cpcloud/ipython-autotime",
+    license="BSD",
+
+    py_modules=["autotime"],
+    zip_safe=True,
+)
